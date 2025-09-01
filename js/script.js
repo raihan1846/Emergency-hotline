f1997227e03980d51894c56167daa6509ecec7eb
raihan1846 <raihan.bd2022@gmail.com>
2025-09-01 18:13:14 +0600

    calling and clear history done

--- js/script.js
+++ js/script.js
@@ -9,3 +9,51 @@ for (const heart of heartBtn) {
         heartCount.textContent = totalHeart;
     })
 }
+
+// call button start 
+
+const totalCoin = document.getElementById('coinText');
+const serverNumber = document.querySelectorAll('.server-number');
+const  serverName = document.querySelectorAll('.server-name');
+const callCoin = document.querySelectorAll('.call');
+
+for (let i = 0; i < callCoin.length; i++) {
+    callCoin[i].addEventListener('click', function () {
+        let coin = totalCoin.textContent;
+        if (coin < 20) {
+            alert("Not Enough Coin!");
+            return;
+        }
+        const sName = serverName[i].textContent;
+        const sNumber = serverNumber[i].textContent;
+        alert(`📞 Calling ${sName} ${sNumber}`)
+        coin = coin - 20;
+        totalCoin.textContent = coin;
+        const now = new Date();
+        const list = document.getElementById('list');
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML = `
+         <div class="flex justify-between bg-gray-50 p-4 rounded-lg items-center">
+                <div>
+                  <h2 class="text-lg font-bold">${sName}</h2>
+                  <h3>${sNumber}</h3>
+                </div>
+                <div>
+                  <p class="text-sm font-semibold">${now.toLocaleTimeString()}</p>
+                </div>
+              </div>    
+        `;
+   
+      list.appendChild(createDiv);
+      
+      
+      
+    })
+}
+
+// clear
+const clear = document.getElementById('clear');
+const list = document.getElementById('list');
+clear.addEventListener('click', function(){
+    list.innerHTML = "";
+})
